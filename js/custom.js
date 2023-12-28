10832132cb37cf324a00188ed19577e9b4154068
sp-glitch <148739294+sp-glitch@users.noreply.github.com>
2023-12-28 16:54:21 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,33 +1,31 @@
-window.addEventListener("scroll", () => {
-  let SCT = window.scrollY;
-  SCT > 0
-    ? document.querySelector("#Header").classList.add("on")
-    : document.querySelector("#Header").classList.remove("on");
-});
-
-var swiper = new Swiper(".main_slide_wrap", {
-  spaceBetween: 30,
-  effect: "fade",
-
-  loop: true,
-  speed: 1000,
-
-  navigation: {
-    nextEl: ".swiper-button-n",
-    prevEl: ".swiper-button-p",
-  },
-
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  autoplay: {
-    // delay: 1000,
-    disableOnInteraction: false,
-  },
-});
-
 $(function () {
+  $(window).on("scroll", () => {
+    let SCT = $(window).scrollTop();
+    SCT > 0 ? $("#Header").addClass("on") : $("#Header").removeClass("on");
+  });
+
+  const swiper = new Swiper(".main_slide_wrap", {
+    spaceBetween: 30,
+    effect: "fade",
+
+    loop: true,
+    speed: 1000,
+
+    navigation: {
+      nextEl: ".swiper-button-n",
+      prevEl: ".swiper-button-p",
+    },
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+      // delay: 1000,
+      disableOnInteraction: false,
+    },
+  });
+
   $(".mopen").on("click", function () {
     $(".gnb").toggleClass("on");
     $(this).find(".hamburger").toggleClass("is-active");
@@ -45,16 +43,15 @@ $(function () {
         },
       },
     ],
-  })
+  });
 
-  $('.main_product .arrows .left').on('click', function(){
+  $(".main_product .arrows .left").on("click", function () {
     // 슬라이드가 뒤로 가: slick 홈페이지 참조
-    $('.product_slide').slick('slickPrev')
-})
+    $(".product_slide").slick("slickPrev");
+  });
 
-
-$('.main_product .arrows .right').on('click', function(){
+  $(".main_product .arrows .right").on("click", function () {
     // 슬라이드가 뒤로 가: slick 홈페이지 참조
-    $('.product_slide').slick('slickNext')
-});
+    $(".product_slide").slick("slickNext");
+  });
 });
